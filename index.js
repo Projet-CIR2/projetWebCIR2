a3f1960c67d23b29aeda32e4996be97c6f7013a0
sebastiendelestret <58606253+sebastiendelestret@users.noreply.github.com>
2021-02-17 11:36:29 +0100

    add mysql

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express()
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
+const mysql = require('mysql');
 app.use(express.static(__dirname + '/front/'));
 
 app.get('/', (req,res) => {
@@ -22,9 +22,9 @@ io.on('connection', (socket) =>{
 
 http.listen(4200, () => {
     console.log("Bijour vous m'entendii ?")
+});
 
 
-// Initialisation de la connexion à la bdd
 
 const con = mysql.createConnection({
     host: "localhost",
