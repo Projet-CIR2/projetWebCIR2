d27db07406b9e770935be67b1478984c337572f8
Arthur Pineau <58333043+Arthur-P0@users.noreply.github.com>
2021-02-17 11:13:39 +0100

    Test Sql
    voir si on y arrive

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const app = express()
 const http = require("express-session")({
     
 })
+
+// Initialisation de la connexion à la bdd
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "compte"
+});
