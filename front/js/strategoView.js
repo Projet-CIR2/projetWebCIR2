9bd27f2907a7610014f7bf27793a0b82fc96197a
Nicolas <46492476+nicolegrimpeur@users.noreply.github.com>
2021-02-24 18:15:55 +0100

    Mise en place communication front/back
    Recherche et mise en place de la communication entre le front et le back

--- front/js/strategoView.js
+++ front/js/strategoView.js
@@ -1,9 +1,25 @@
 class StrategoView {
     constructor() {
+        this.createListeners();
+    }
 
+    createListeners() {
+        let currentDiv = document.getElementById('plateau');
+        console.log(currentDiv);
+        // currentDiv.addEventListener('click', () => {
+        //     this.clickEvent(0, 0);
+        // });
+        for (let i = 0; i < 10; ++i) {
+            for (let j = 0; j < 10; ++j) {
+                currentDiv.rows[i].cells[j].addEventListener('click', () => {
+                    this.clickEvent(i, j);
+                });
+            }
+        }
     }
 
     clickEvent(x, y) {
-
+        console.log('click en :', x, y);
+        socket.emit('play', x, y);
     }
 }
