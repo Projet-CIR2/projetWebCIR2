66edadbc254958e9dd612254c3c20a17780c881c
Arthur Pineau <58333043+Arthur-P0@users.noreply.github.com>
2021-03-07 16:24:54 +0100

    Update login.js

--- front/js/login.js
+++ front/js/login.js
@@ -1,9 +1,9 @@
 let form = document.getElementById('form');
-let name = document.getElementById('pseudo');
+let username = document.getElementById('pseudo');
 let pwrd = document.getElementById('password');
 
 form.addEventListener('submit', event=> {
 
     event.preventDefault();
-    logger.sendLogin(name.value, pwrd.value);
+    logger.sendLogin(username.value, pwrd.value);
 });
