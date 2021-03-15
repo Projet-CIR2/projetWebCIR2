f15eb6b0ba69224fe87be9c6a4d3198a6e050c07
Nicolas <46492476+nicolegrimpeur@users.noreply.github.com>
2021-03-15 19:30:10 +0100

    Mise au propre du module des sockets

--- back/modules/initSocket.js
+++ back/modules/initSocket.js
@@ -1,25 +1,4 @@
-// module.exports = {
-//     initSocket(socket, game) {
-//         socket.on('play', (x, y) => {
-//             game.play(x, y);
-//         });
-//         socket.on('move', (x1, y1) => {
-//             console.log(x1, y1);
-//         });
-//     }
-// }
-
-
-
 let socketModule = (function () {
-    // l'objet args correspond aux arguments qui seront appelés par le callBack
-    // exemple pour play, args[0] correspond à x et args[1] correspond à y
-    // en réalité ça ne sert algorithmiquement pas à grand chose, mais il est la pour plus de lisibilité
-    let tabSockets = [
-        {id:'play', args:'(x, y)', callBack:'game.play(args[0], args[1])'},
-        {id:'move', args:'', callBack:'console.log("move")'}
-    ];
-
     function play(socket, game) {
         socket.on('play', (x, y) => {
             game.play(x, y);
@@ -34,14 +13,8 @@ let socketModule = (function () {
 
     return {
         initSocket(socket, game) {
-            for (let unSocket of tabSockets) {
-                socket.on(unSocket.id, (...args) => {
-                    eval(unSocket.callBack);
-                });
-            }
-
-            // play(socket, game);
-            // move(socket, game);
+            play(socket, game);
+            move(socket, game);
         }
     }
 }) ();
