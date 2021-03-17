e43ffdb9594fcc774990f676662c4d252022f413
Arthur Pineau <58333043+Arthur-P0@users.noreply.github.com>
2021-03-17 12:05:57 +0100

    Check Compte proto

--- back/checkConnection.js
+++ back/checkConnection.js
@@ -1,8 +1,18 @@
 let checkConnection = (function(sql, username, psswrd){
 
-    return {
-        register(){
-            console.log("ooooof");
+    let sql = "SELECT *FROM session WHERE Pseudo = ? AND Mdp = ?"
+
+    return{
+        verify(){
+
+            con.query(sql,username,psswrd,(err) => {
+                if (!err.isEmpty()) {
+                    console.log("Pseudo ou Mdp incorrect");
+                }
+                else{
+                    console.log("T'es connecté sale Bg");
+                }
+            });
         }
     }
 })();
