2ac2e383367f05b6a341d8f1846b0f57d281b2d8
Nicolas <46492476+nicolegrimpeur@users.noreply.github.com>
2021-03-17 12:09:20 +0100

    Majuscule au Joueur.js

--- front/js/main.js
+++ front/js/main.js
@@ -1,5 +1,11 @@
 let main = function () {
     let view = new StrategoView();
     socketModule.initSocket(socket, view);
+
+    // let game =
+    testUnitaire();
 };
 
+let testUnitaire = function () {
+
+}
