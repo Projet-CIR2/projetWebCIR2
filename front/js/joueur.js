5edbcad798a7ddc251f022b0378b0615017bcf71
Califano9577 <amaury.giot@student.yncrea.fr>
2021-03-17 11:41:08 +0100

    rajout classe joueur

---
+++ front/js/joueur.js
@@ -0,0 +1,8 @@
+//la classe pour les joueur
+class Joueur {
+    constructor(couleur) {
+        this.color = couleur;//0 blanc, 1 noir
+        this.points = 0;//nombre de points
+        this.pret = 0;//pret pour le debut de la parti
+    }
+}
