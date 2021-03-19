de107270dff213a65641cf6e40ff156348b9329c
Nicolas <46492476+nicolegrimpeur@users.noreply.github.com>
2021-03-19 11:26:22 +0100

    Tests enlevés

--- back/models/stratego.js
+++ back/models/stratego.js
@@ -1,9 +1,9 @@
 class Stratego {
     constructor(socket) {
         this.socket = socket;
-        this.socket.emit('removePions');
-        this.socket.emit('affichePion', 'espion', 2, 5);
-        this.socket.emit('removePion', 5, 5);
+        // this.socket.emit('removePions');
+        // this.socket.emit('affichePion', 'espion', 2, 5);
+        // this.socket.emit('removePion', 5, 5);
 
     }
 
