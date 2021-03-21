e811538c5c2d0f6f29308f509592eab6f40f2d30
Arthur Pineau <58333043+Arthur-P0@users.noreply.github.com>
2021-03-21 17:53:31 +0100

    Update checkConnection.js

--- back/connection/checkConnection.js
+++ back/connection/checkConnection.js
@@ -25,10 +25,14 @@ module.exports = {
     },
 
 
-    createAccount(con, username, psswrd){
+    createAccount(con, values){
         
-    }
+    let sql = "INSERT INTO session SET ?";  
+        con.query(sql, values, (err, result) => {
+            if (err) throw err;
+            console.log("One Session inserted");
+        });
 
-    
+    }
 
 }
