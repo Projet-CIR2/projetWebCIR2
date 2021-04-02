b854493718a129660dfe1c477851debe5fc20328
Nicolas Barrat <nicolas.barrat@student.junia.com>
2021-04-02 11:18:32 +0200

    Matchmaking plus ou moins fini mais ça marche

--- back/models/room.js
+++ back/models/room.js
@@ -1,13 +1,15 @@
-
 const Stratego = require('./stratego');
 const init = require('../modules/initSocket');
 
 class Room{
-    constructor(socket, player1_, player2_){
+    constructor(socket, io, token, player1_, player2_){
         let player1 = player1_;
         let player2 = player2_;
-        // socket.broadcast.to(player1).emit('lanceJeu');
-        console.log("Nouvelle partie entre ", player1, " et ", player2 );
+        if (player1 === player2) {
+            player1 += ' 1';
+            player2 += ' 2';
+        }
+        io.to(token).emit('hey', 'je suis dans room');
         // const game = new Stratego(socket, "j1", "j2");
         // init.initSocket(socket, game)
       
